9b2584a9c3ee298fccc5e0ada037f68948a3ae68
Johan Tjelldén <johan@hedvig.com>
2017-10-11 14:14:25 +0200

    HelloHedvig should accept POST requests.

--- src/main/java/com/hedvig/gateway/RedirectController.java
+++ src/main/java/com/hedvig/gateway/RedirectController.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import sun.misc.BASE64Encoder;
 
@@ -29,11 +26,11 @@ public class RedirectController {
 
 	private static ConcurrentHashMap<UUID, String> collectMap = new ConcurrentHashMap<>();
 	
-	@GetMapping("/helloHedvig")
+	@PostMapping("/helloHedvig")
 	String login() throws NoSuchAlgorithmException {
 
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<HelloHedvigResponse> response = restTemplate.getForEntity("http://localhost:4084/member/helloHedvig", HelloHedvigResponse.class);
+		ResponseEntity<HelloHedvigResponse> response = restTemplate.postForEntity("http://localhost:4084/member/helloHedvig", HelloHedvigResponse.class);
 
 		String jwt = createJWT();
 		assignJWT(jwt, response.getBody().memberId.toString());
