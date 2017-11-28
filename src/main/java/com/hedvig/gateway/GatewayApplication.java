769c3726a2115c9151bd13610323bca717f25af0
Johan Tjelldén <johan@hedvig.com>
2017-11-28 13:52:12 +0100

    Add CORS filter

--- src/main/java/com/hedvig/gateway/GatewayApplication.java
+++ src/main/java/com/hedvig/gateway/GatewayApplication.java
@@ -11,6 +11,9 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.TreeMap;
 
@@ -41,6 +44,20 @@ public class GatewayApplication {
 	@Bean
 	public RestTemplate restTemplate() {
     	return new RestTemplate();
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("POST");
+		config.setMaxAge(3000l);
+		config.addAllowedHeader("Authorization");
+		config.addExposedHeader("location");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 	/*
