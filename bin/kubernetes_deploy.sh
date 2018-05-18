e0cd33cd54730f8c26db4b6cf5279c185dffaf96
Johan Tjelldén <johan@hedvig.com>
2018-05-18 15:59:51 +0200

    Download authenticator from s3

--- bin/kubernetes_deploy.sh
+++ bin/kubernetes_deploy.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-export GOPATH="${TRAVIS_BUILD_DIR}/Godeps/_workspace:$GOPATH"
-export PATH="${TRAVIS_BUILD_DIR}/Godeps/_workspace/bin:$PATH"
+export PATH="~/.kube:$PATH"
 
 aws s3 cp s3://dev-com-hedvig-cluster-ett-data/kube ~/.kube --recursive
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-
-go get -u -v github.com/heptio/authenticator/cmd/heptio-authenticator-aws
 
 ./kubectl set image deployment/api-gateway api-gateway=$REMOTE_IMAGE_URL:$TRAVIS_BUILD_NUMBER
