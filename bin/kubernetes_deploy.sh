c26c36f9b4c3f10ad761062f6f3b4729d75a6239
Johan Tjelldén <johan@hedvig.com>
2018-05-18 15:49:06 +0200

    Add GOPATHS

--- bin/kubernetes_deploy.sh
+++ bin/kubernetes_deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export GOPATH="${TRAVIS_BUILD_DIR}/Godeps/_workspace:$GOPATH"
+export PATH="${TRAVIS_BUILD_DIR}/Godeps/_workspace/bin:$PATH"
+
 aws s3 cp s3://dev-com-hedvig-cluster-ett-data/kube ~/.kube --recursive
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
