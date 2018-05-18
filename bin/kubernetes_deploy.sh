5d7cf1e8501accd5800054c342ffc80a02bc32e5
Johan Tjelldén <johan@hedvig.com>
2018-05-18 17:51:56 +0200

    Trying to get heptio to work on travis

--- bin/kubernetes_deploy.sh
+++ bin/kubernetes_deploy.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-export PATH="~/.kube:$PATH"
-
 aws s3 cp s3://dev-com-hedvig-cluster-ett-data/kube ~/.kube --recursive
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-chmod +x ~/.kube/heptio-authenticator-aws
+mv ~/.kube/heptio-authenticator-aws ~/
+chmod +x ~/heptio-authenticator-aws
 
-./kubectl set image deployment/api-gateway api-gateway=$REMOTE_IMAGE_URL:$TRAVIS_BUILD_NUMBER
+PATH="~:$PATH" ./kubectl set image deployment/api-gateway api-gateway=$REMOTE_IMAGE_URL:$TRAVIS_BUILD_NUMBER
