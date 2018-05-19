cc42c9589a3051346f436a4e884e13d061ad2574
Johan Tjelldén <johan@hedvig.com>
2018-05-19 12:44:04 +0200

    Will this make travis work?

--- bin/kubernetes_deploy.sh
+++ bin/kubernetes_deploy.sh
@@ -4,7 +4,6 @@ aws s3 cp s3://dev-com-hedvig-cluster-ett-data/kube ~/.kube --recursive
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv ~/.kube/heptio-authenticator-aws ~/
-chmod +x ~/heptio-authenticator-aws
+chmod +x ~/.kube/heptio-authenticator-aws
 
-PATH="~:$PATH" ./kubectl set image deployment/api-gateway api-gateway=$REMOTE_IMAGE_URL:$TRAVIS_BUILD_NUMBER
+./kubectl set image deployment/api-gateway api-gateway=$REMOTE_IMAGE_URL:$TRAVIS_BUILD_NUMBER
