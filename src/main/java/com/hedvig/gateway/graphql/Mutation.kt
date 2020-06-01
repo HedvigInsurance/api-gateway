e888d00e17c5ec456850e6867e1ed939b2cf5698
meletios <meletios@hedvig.com>
2020-06-01 17:09:55 +0200

    use exchangeService on mutation

--- src/main/java/com/hedvig/gateway/graphql/Mutation.kt
+++ src/main/java/com/hedvig/gateway/graphql/Mutation.kt
@@ -3,11 +3,16 @@ package com.hedvig.gateway.graphql
 import com.coxautodev.graphql.tools.GraphQLMutationResolver
 import com.hedvig.gateway.graphql.types.ExchangeTokenInput
 import com.hedvig.gateway.graphql.types.ExchangeTokenResponse
+import com.hedvig.gateway.service.ExchangeService
 import org.springframework.stereotype.Component
 
 @Component
-class Mutation() : GraphQLMutationResolver {
+class Mutation(
+  val exchangeService: ExchangeService
+) : GraphQLMutationResolver {
   fun exchangeToken(input: ExchangeTokenInput): ExchangeTokenResponse {
-    return ExchangeTokenResponse.ExchangeTokenFailureResponse("No reason")
+    val token = exchangeService.exchangeToken(input.exchangeToken)
+      ?: return ExchangeTokenResponse.ExchangeTokenFailureResponse("No token found")
+    return ExchangeTokenResponse.ExchangeTokenSuccessResponse(token)
   }
 }
