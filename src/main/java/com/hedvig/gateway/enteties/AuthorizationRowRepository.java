5b0bd6d95b6c6bda4cd6f0e5d2053bdc1f835fca
meletios <meletios@hedvig.com>
2020-06-02 16:40:31 +0200

    revert kotlinizing

---
+++ src/main/java/com/hedvig/gateway/enteties/AuthorizationRowRepository.java
@@ -0,0 +1,9 @@
+package com.hedvig.gateway.enteties;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorizationRowRepository extends CrudRepository<AuthorizationRow, String> {
+    AuthorizationRow findByMemberId(String memberId);
+}
--- src/main/java/com/hedvig/gateway/enteties/AuthorizationRowRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.hedvig.gateway.enteties
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface AuthorizationRowRepository : CrudRepository<AuthorizationRow, String> {
-  fun findByMemberId(memberId: String): AuthorizationRow?
-}
