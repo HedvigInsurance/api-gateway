a11071cd80f41db9778a0b1e979c3354c4746fd3
meletios <meletios@hedvig.com>
2020-06-02 13:14:36 +0200

    use secure random

--- src/main/java/com/hedvig/gateway/service/TokenServiceImpl.kt
+++ src/main/java/com/hedvig/gateway/service/TokenServiceImpl.kt
@@ -1,15 +1,15 @@
 package com.hedvig.gateway.service
 
 import org.springframework.stereotype.Service
+import java.security.SecureRandom
 import java.util.Base64
-import java.util.Random
 
 @Service
 class TokenServiceImpl : TokenService {
   override fun createJWT(): String {
     var jwt = ""
     for (i in 1..3) {
-      val r = Random()
+      val r = SecureRandom()
       val bytes = ByteArray(10)
       val adapter = Base64.getEncoder()
       r.nextBytes(bytes)
