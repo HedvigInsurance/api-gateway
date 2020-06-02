3a8d8ec7b396baf2341b8f923cfb0ec9249c38c8
meletios <meletios@hedvig.com>
2020-06-02 13:13:56 +0200

    refactor table

--- src/main/java/com/hedvig/gateway/enteties/ExchangeToken.kt
+++ src/main/java/com/hedvig/gateway/enteties/ExchangeToken.kt
@@ -1,24 +1,24 @@
 package com.hedvig.gateway.enteties
 
 import org.hibernate.annotations.CreationTimestamp
-import org.hibernate.annotations.UpdateTimestamp
 import java.time.Instant
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.Table
+import javax.persistence.UniqueConstraint
 
 @Entity
+@Table(uniqueConstraints = [UniqueConstraint(columnNames = ["token"])])
 class ExchangeToken(
-  @Id
+  @Id @GeneratedValue
+  val id: Long,
   val token: String,
   val memberId: String,
   val validity: Long
 ) {
   @CreationTimestamp
   var createdAt: Instant = Instant.now()
-
-  @UpdateTimestamp
-  var updatedAt: Instant = Instant.now()
-
 
   fun isValid(): Boolean {
     return createdAt.plusSeconds(validity) >= Instant.now()
