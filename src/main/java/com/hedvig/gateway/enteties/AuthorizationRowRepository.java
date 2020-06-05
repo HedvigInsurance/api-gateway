8eaffdfef3b566facdbf3efa50ed4101e66dabcc
meletios <meletios@hedvig.com>
2020-06-05 13:45:18 +0200

    cleaning

--- src/main/java/com/hedvig/gateway/enteties/AuthorizationRowRepository.java
+++ src/main/java/com/hedvig/gateway/enteties/AuthorizationRowRepository.java
@@ -3,8 +3,6 @@ package com.hedvig.gateway.enteties;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AuthorizationRowRepository extends CrudRepository<AuthorizationRow, String> {
     AuthorizationRow findTopByMemberIdOrderByCreatedAtDesc(String memberId);
